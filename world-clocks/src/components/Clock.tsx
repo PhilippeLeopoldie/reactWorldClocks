1ca3c3d073c51554028ccbac7aa9d48889ca4251
PhilippeLeopoldie <107151764+PhilippeLeopoldie@users.noreply.github.com>
2023-02-27 13:08:49 +0100

    implement setTimeOut versus useEffect

--- world-clocks/src/components/Clock.tsx
+++ world-clocks/src/components/Clock.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 
 const timeFunc = (time : Date) => {
     return (`${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`)
 }
+
 const Clock = () => {
     const [time, setTime] = useState(new Date())
+    /* useEffect(() => {
+        setTime(new Date())
+    },[time]) */
+    setTimeout(() => {
+        setTime(new Date())
+    },1000)
     return (<h1>{timeFunc(time)}</h1>);
 }
 
