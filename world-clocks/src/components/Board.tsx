6bd88ee534b91a4b0eaa476cc58bae9391b3b708
Kimi Andersson <kimianderssonmusic@gmail.com>
2023-02-27 15:44:36 +0100

    FInished lab moving to extras

--- world-clocks/src/components/Board.tsx
+++ world-clocks/src/components/Board.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Clock from "./Clock";
 import './BoardStyle.css';
 
 const Board = () => {
+    const clockArr = [
+        ['America/New_York', 'New York'],
+        ['Europe/Paris', 'Paris'],
+        ['Europe/Stockholm', 'Stockholm'],
+        ['Asia/Tokyo', 'Tokyo'],
+        ['Asia/Dubai', 'Dubai']];
+
+    const [clocks, addClock] = useState(clockArr);
     return (
         <div className='boarderContainer'>
         <div className="title">World Clocks</div>
-        <Clock timeZone ='America/New_York' city='New York'/>
-        <Clock timeZone ='Europe/Paris' city='Paris'/>
-        <Clock timeZone ='Europe/Stockholm' city='Stockholm'/>
-        <Clock timeZone ='Asia/Tokyo' city='Tokyo'/>
-        <Clock timeZone ='Asia/Dubai' city='Dubai'/>
+            <form>
+                <label>
+                    <input name="city" type={"text"}/>
+                </label>
+                <label htmlFor={"timezone"}>
+                    <input name="timezone" type={"text"}/>
+                </label>
+                <button onClick={addClocks} type={"submit"}>Add Clock</button>
+            </form>
+            <div className="clockContainer">
+                {clocks.map((clock, index) => {
+                    return (<Clock key={index} timeZone={clock[0]} city={clock[1]}/>)
+                }) }
+            </div>
         </div>
     )
+}
+
+const addClocks = (e : any) => {
+    e.preventDefault();
+    console.log(e.target);
 }
 
 export default Board;
